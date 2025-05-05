12704903eb2d6e268499611293e0628d46291564
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-05-05 14:06:35 +0100

    msg

--- src/app/dashboard/orders/page.tsx
+++
@@ -1,9 +0,0 @@
-import StoreOrderList from "@/components/dashboard/orders/StoreOrderList";
-
-export default function StoreOrdersPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <StoreOrderList />
-    </div>
-  );
-} 
--- src/app/track/page.tsx
+++
@@ -1,9 +0,0 @@
-import OrderTracking from "@/components/orders/tracking/OrderTracking";
-
-export default function TrackOrderPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <OrderTracking />
-    </div>
-  );
-} 
