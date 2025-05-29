8dd16ea69ec20a299243ebfe63cf7202002b7436
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-05-29 22:37:09 +0100

    msg

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,5 +1,10 @@
+import React, { Suspense } from "react";
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 
 export default function ResetPasswordPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense fallback={<div>Loading form...</div>}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
