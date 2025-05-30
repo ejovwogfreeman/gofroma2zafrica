e9ae7eabd9d9b22108e9b32d78d1d25e304d0634
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2025-05-30 09:56:20 +0100

    msg

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -1,12 +1,24 @@
+// "use client";
+
+// import { Metadata } from "next";
+// import AccountLayout from "@/components/account/layout/AccountLayout";
+
+// export const metadata: Metadata = {
+//   title: "Account Dashboard | GoFromA2zAfrica",
+//   description: "Manage your account, orders, and preferences",
+// };
+
+// export default function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <div className="min-h-screen bg-dark-primary">
+//       <AccountLayout>{children}</AccountLayout>
+//     </div>
+//   );
+// }
+
 "use client";
 
-import { Metadata } from "next";
 import AccountLayout from "@/components/account/layout/AccountLayout";
-
-export const metadata: Metadata = {
-  title: "Account Dashboard | GoFromA2zAfrica",
-  description: "Manage your account, orders, and preferences",
-};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
